8bcf5c92c751d399e288edef50705c3f4dc5a493
Esa Junttila <esa.junttila@gmail.com>
2013-07-13 17:52:11 +0300

    add a general-use function for inverting list-mappings

---
+++ src/imdb_list_analyzer/common.clj
@@ -0,0 +1,29 @@
+; Common functions to be used anywhere in the
+; imdb-list-analyzer program.
+
+(ns imdb-list-analyzer.common)
+ 
+(defn invert-multimap
+  [rate-lists]
+  "Invert a mapping from (duplicate) keys to lists:
+   take as input keys and lists of associated values;
+   return a hash-map where distinct values are mapped to a list of original keys.;
+
+  Example:
+    Original mappings:
+      1 : [a b c]
+      2 : [a]
+      2 : [a b]
+      3 : [c d]
+      3 : []
+    As input:
+      (def input [ [1 [:a :b :c]] [2 [:a]] [2 [:a :b]] [3 [:c :d]] [3 []]])
+    Result from (invert-multimap input):
+      {:d [3], :c [1 3], :b [1 2], :a [1 2 2]}
+  "
+  (let [uniq (distinct (flatten (map second rate-lists)))]
+    (reduce
+      (fn [coll, val] (merge-with conj coll val))
+	  (zipmap uniq (repeat (count uniq) []))
+      (for [[key val-list] rate-lists, val val-list] {val key}))))
+
