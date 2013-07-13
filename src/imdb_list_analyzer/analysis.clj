bec4436a1d2f95dcf16cfbe7e94eb8db88707166
Esa Junttila <esa.junttila@gmail.com>
2013-07-14 01:29:17 +0300

    add statistical test for identifying best and worst directors

--- src/imdb_list_analyzer/analysis.clj
+++ src/imdb_list_analyzer/analysis.clj
@@ -59,3 +59,50 @@
   (com/invert-multimap
     (map #(vector (:rate %) (:directors %)) titles-coll)))
 
+(defn sample-null-ref-value
+  [num-values emp-distr]
+  (mtools/mean (take num-values (mtools/sample-distr emp-distr))))
+
+(def num-samples 1000)
+
+(defn compute-reference-value
+  [rates emp-distr distr-mu]
+  (let [mu (mtools/mean rates)
+        num (count rates)
+        samples (take num-samples (repeatedly #(sample-null-ref-value num emp-distr)))]
+    (/ (count (filter #(if (<= distr-mu mu) (< % mu) (<= % mu)) samples)) (count samples))))
+
+(defn director-empirical-rank
+  [director-rate-lists emp-distr]
+  (let [distr-mu (mtools/mean (map first (:symbcumuprobs emp-distr)))]
+    (map #(compute-reference-value % emp-distr distr-mu) director-rate-lists)))
+
+(defn director-rank
+  [titles-coll]
+  (let [dirs (rating-directors titles-coll)
+        emp-distr (mtools/gen-emp-distr (map :rate titles-coll))]
+    (map vector dirs (director-empirical-rank (map second dirs) emp-distr))))
+
+(defn small-test
+  []
+  (let [coll (rest (imdb/read-imdb-data "resources/example_ratings.csv"))
+        rank (reverse (sort-by second (director-rank coll)))]
+    (do
+      (println "The best:")
+      (let [best (take 10 rank)]
+        (do
+          (println "Director, Rank-p-value, Rates\n---------------------------")
+          (doseq [[[dir rates] rank-val] best] (println (str dir ", " (double rank-val) ", " rates)))))
+      (println "The worst:")
+      (let [worst (take-last 10 rank)]
+        (do
+          (println "Director, Rank-p-value, Rates\n---------------------------")
+          (doseq [[[dir rates] rank-val] worst] (println (str dir ", " (double rank-val) ", " rates))))))
+    ;(rating-directors coll)
+    ;(mtools/gen-emp-distr (map :rate coll))
+    ;(reverse (sort-by second (director-rank coll)))
+    ;(director-empirical-rank [[2 2 1] [1 2 3 4] [4] [1]] (mtools/gen-emp-distr [1 1 1 2 3 3 4 4 4 4]))
+    ;(compute-reference-value [1 1 2 2 3 4 4 4 3 3] (mtools/gen-emp-distr [1 1 1 2 3 3 4 4 4 4]) (/ 27 10))
+    ;(take 100 (repeatedly #(sample-null-ref-value 4 (mtools/gen-emp-distr [1 1 1 2 3 3 4 4 4 4]))))
+))
+
