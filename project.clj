de1277624dc003d0a5511b696ec3e54c9d66cb75
Esa Junttila <esa.junttila@gmail.com>
2015-11-03 21:20:20 +0200

    switch project env to Clojure version 1.7.0 (to enable cljs)

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject imdb-list-analyzer "0.1.0-SNAPSHOT"
   :description "Tool for analyzing IMDb rating lists"
-  :url "http://github.com/Eski-or-die/imdb-list-analyzer"
+  :url "http://github.com/dresa/imdb-list-analyzer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [clojure-csv/clojure-csv "2.0.1"]]
   :main imdb-list-analyzer.core)
